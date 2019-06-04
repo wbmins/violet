e6e7c9fca0ace99824acf1725b138e1bfa11d529
Restyled.io <commits@restyled.io>
2019-06-04 07:56:37 +0000

    Restyled by prettier

--- assets/js/_custom.js
+++ assets/js/_custom.js
@@ -3,8 +3,14 @@
 // ==============================
 
 // 顶部阅读进度条
-$(document).ready(function () {
-    $(window).scroll(function () {
-        $(".top-scroll-bar").attr("style", "width: " + ($(this).scrollTop() / ($(document).height() - $(this).height()) * 100) + "%; display: block;");
-    });
+$(document).ready(function() {
+  $(window).scroll(function() {
+    $(".top-scroll-bar").attr(
+      "style",
+      "width: " +
+        ($(this).scrollTop() / ($(document).height() - $(this).height())) *
+          100 +
+        "%; display: block;"
+    );
+  });
 });
