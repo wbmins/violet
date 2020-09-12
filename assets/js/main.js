37a176946a2d8a2b1b5f5210b88bd8aa2d0e740f
EZ2003007 <wbmins@qq.com>
2020-09-12 16:42:34 +0800

    nothing

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,7 +63,7 @@ jQuery(function ($) {
   _Blog.articleToc = function () {
     window.onload = function () {
       var fix = $(".post-toc");
-      var end = $(".post-comment");
+      var end = $(".post-tags");
       var fixTop = fix.offset().top,
         fixHeight = fix.height();
       var endTop, miss;
